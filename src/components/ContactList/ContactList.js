17ddbd0830fc767bdb61d7106b1ec01e439111b1
Maryna Danyliuk <maruna.danulyk@gmail.com>
2023-08-23 17:40:10 +0300

    hometask

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import { RiDeleteBin2Line } from 'react-icons/ri';
+
+export const ContactList = ({ contacts, onDelete }) => {
+  return (
+    <ul>
+      {contacts.map(contact => {
+        return (
+          <li key={contact.id}>
+            <p>
+              {contact.name}: <span>{contact.number}</span>
+            </p>
+            <button type="button" onClick={() => onDelete(contact.id)}>
+             Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
